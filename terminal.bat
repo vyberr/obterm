8870efeaa30d769bb52dc6378cbc2c6430a20e95
vyberr <92124191+vyberr@users.noreply.github.com>
2022-05-22 01:15:47 -0600

    Add files via upload

---
+++ terminal.bat
@@ -0,0 +1,437 @@
+@echo off
+:: Editable Vars:
+::"if you would like to customize your terminal, please only change these variables"
+set version=1.0
+set name=Vyberr
+set companyname=NulBerry
+set github=github.com/vyberr
+:: end of editable vars!
+::start of code.
+::
+:: ___  __    _______   _______   ________        ________  ___  ___  _________   
+::|\  \|\  \ |\  ___ \ |\  ___ \ |\   __  \      |\   __  \|\  \|\  \|\___   ___\ 
+::\ \  \/  /|\ \   __/|\ \   __/|\ \  \|\  \     \ \  \|\  \ \  \\\  \|___ \  \_| 
+:: \ \   ___  \ \  \_|/_\ \  \_|/_\ \   ____\     \ \  \\\  \ \  \\\  \   \ \  \  
+::  \ \  \\ \  \ \  \_|\ \ \  \_|\ \ \  \___|      \ \  \\\  \ \  \\\  \   \ \  \ 
+::   \ \__\\ \__\ \_______\ \_______\ \__\          \ \_______\ \_______\   \ \__\
+::    \|__| \|__|\|_______|\|_______|\|__|           \|_______|\|_______|    \|__|
+::                                                                              
+:: ___  __    _______   _______   ________        ________  ___  ___  _________   
+::|\  \|\  \ |\  ___ \ |\  ___ \ |\   __  \      |\   __  \|\  \|\  \|\___   ___\ 
+::\ \  \/  /|\ \   __/|\ \   __/|\ \  \|\  \     \ \  \|\  \ \  \\\  \|___ \  \_| 
+:: \ \   ___  \ \  \_|/_\ \  \_|/_\ \   ____\     \ \  \\\  \ \  \\\  \   \ \  \  
+::  \ \  \\ \  \ \  \_|\ \ \  \_|\ \ \  \___|      \ \  \\\  \ \  \\\  \   \ \  \ 
+::   \ \__\\ \__\ \_______\ \_______\ \__\          \ \_______\ \_______\   \ \__\
+::    \|__| \|__|\|_______|\|_______|\|__|           \|_______|\|_______|    \|__|
+::                                                                              
+:: ___  __    _______   _______   ________        ________  ___  ___  _________   
+::|\  \|\  \ |\  ___ \ |\  ___ \ |\   __  \      |\   __  \|\  \|\  \|\___   ___\ 
+::\ \  \/  /|\ \   __/|\ \   __/|\ \  \|\  \     \ \  \|\  \ \  \\\  \|___ \  \_| 
+:: \ \   ___  \ \  \_|/_\ \  \_|/_\ \   ____\     \ \  \\\  \ \  \\\  \   \ \  \  
+::  \ \  \\ \  \ \  \_|\ \ \  \_|\ \ \  \___|      \ \  \\\  \ \  \\\  \   \ \  \ 
+::   \ \__\\ \__\ \_______\ \_______\ \__\          \ \_______\ \_______\   \ \__\
+::    \|__| \|__|\|_______|\|_______|\|__|           \|_______|\|_______|    \|__|
+::                                                                              
+::
+:: _____ ______   ________  ___  ________           ________  ________  ________  _______      
+::|\   _ \  _   \|\   __  \|\  \|\   ___  \        |\   ____\|\   __  \|\   ___ \|\  ___ \     
+::\ \  \\\__\ \  \ \  \|\  \ \  \ \  \\ \  \       \ \  \___|\ \  \|\  \ \  \_|\ \ \   __/|    
+:: \ \  \\|__| \  \ \   __  \ \  \ \  \\ \  \       \ \  \    \ \  \\\  \ \  \ \\ \ \  \_|/__  
+::  \ \  \    \ \  \ \  \ \  \ \  \ \  \\ \  \       \ \  \____\ \  \\\  \ \  \_\\ \ \  \_|\ \ 
+::   \ \__\    \ \__\ \__\ \__\ \__\ \__\\ \__\       \ \_______\ \_______\ \_______\ \_______\
+::    \|__|     \|__|\|__|\|__|\|__|\|__| \|__|        \|_______|\|_______|\|_______|\|_______|
+::
+:: _____ ______   ________  ___  ________           ________  ________  ________  _______      
+::|\   _ \  _   \|\   __  \|\  \|\   ___  \        |\   ____\|\   __  \|\   ___ \|\  ___ \     
+::\ \  \\\__\ \  \ \  \|\  \ \  \ \  \\ \  \       \ \  \___|\ \  \|\  \ \  \_|\ \ \   __/|    
+:: \ \  \\|__| \  \ \   __  \ \  \ \  \\ \  \       \ \  \    \ \  \\\  \ \  \ \\ \ \  \_|/__  
+::  \ \  \    \ \  \ \  \ \  \ \  \ \  \\ \  \       \ \  \____\ \  \\\  \ \  \_\\ \ \  \_|\ \ 
+::   \ \__\    \ \__\ \__\ \__\ \__\ \__\\ \__\       \ \_______\ \_______\ \_______\ \_______\
+::    \|__|     \|__|\|__|\|__|\|__|\|__| \|__|        \|_______|\|_______|\|_______|\|_______|
+::
+:: _____ ______   ________  ___  ________           ________  ________  ________  _______      
+::|\   _ \  _   \|\   __  \|\  \|\   ___  \        |\   ____\|\   __  \|\   ___ \|\  ___ \     
+::\ \  \\\__\ \  \ \  \|\  \ \  \ \  \\ \  \       \ \  \___|\ \  \|\  \ \  \_|\ \ \   __/|    
+:: \ \  \\|__| \  \ \   __  \ \  \ \  \\ \  \       \ \  \    \ \  \\\  \ \  \ \\ \ \  \_|/__  
+::  \ \  \    \ \  \ \  \ \  \ \  \ \  \\ \  \       \ \  \____\ \  \\\  \ \  \_\\ \ \  \_|\ \ 
+::   \ \__\    \ \__\ \__\ \__\ \__\ \__\\ \__\       \ \_______\ \_______\ \_______\ \_______\
+::    \|__|     \|__|\|__|\|__|\|__|\|__| \|__|        \|_______|\|_______|\|_______|\|_______|
+::
+:: _____ ______   ________  ___  ________           ________  ________  ________  _______      
+::|\   _ \  _   \|\   __  \|\  \|\   ___  \        |\   ____\|\   __  \|\   ___ \|\  ___ \     
+::\ \  \\\__\ \  \ \  \|\  \ \  \ \  \\ \  \       \ \  \___|\ \  \|\  \ \  \_|\ \ \   __/|    
+:: \ \  \\|__| \  \ \   __  \ \  \ \  \\ \  \       \ \  \    \ \  \\\  \ \  \ \\ \ \  \_|/__  
+::  \ \  \    \ \  \ \  \ \  \ \  \ \  \\ \  \       \ \  \____\ \  \\\  \ \  \_\\ \ \  \_|\ \ 
+::   \ \__\    \ \__\ \__\ \__\ \__\ \__\\ \__\       \ \_______\ \_______\ \_______\ \_______\
+::    \|__|     \|__|\|__|\|__|\|__|\|__| \|__|        \|_______|\|_______|\|_______|\|_______|
+                                                                                             
+                                                                                             
+                                                                                             
+
+::                                                                              
+chcp 65001
+cls
+mode 90,50
+title NimBus Terminal %date% %time%
+:main
+echo.  
+echo [40;34m╔╗╔╦╔╦╗╔╗ ╦ ╦╔═╗  ┌┬┐┌─┐┬─┐┌┬┐┬┌┐┌┌─┐┬     [40;37m│ Date: %date%    [40;37m│ %name%
+echo [40;34m║║║║║║║╠╩╗║ ║╚═╗   │ ├┤ ├┬┘│││││││├─┤│     [40;37m│ Time: %time%   [40;37m│ %companyname%
+echo [40;34m╝╚╝╩╩ ╩╚═╝╚═╝╚═╝   ┴ └─┘┴└─┴ ┴┴┘└┘┴ ┴┴─┘   [40;37m│ User: %username%         [40;37m│ %github%
+echo. 
+:commandline
+echo.
+set command=
+set /p command=[40;34m$root:[40;37m
+::
+:: - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+::
+rem commandlist >
+if %command%==help goto help
+if %command%==cls goto clearlist
+if %command%==search goto search
+if %command%==searchgoogle goto search
+if %command%==searchedge goto searchedge
+if %command%==searchurl goto searchurl
+if %command%==startapp goto startapp
+if %command%==start goto start
+if %command%==shutdownlan start shutdown -i && goto commandline
+if %command%==setwinsize goto setsize
+if %command%==setwinsizelong goto setsizelong
+if %command%==setwinsizeshort goto setsizeshort
+if %command%==del goto del
+if %command%==writetxt goto writetxt
+if %command%==erasetxt goto erasetxt
+if %command%==install goto startinstall
+if %command%==iptool goto launchip
+if %command%==nettool goto launchnet
+if %command%==pingtool goto launchping
+if %command%==allfiles goto allfiles
+if %command%==rubx3 goto secret
+if %command%==debugmode goto debugmode
+if %command%==scriptswrite goto scriptwrite
+if %command%==terminalmini goto terminalmini
+if %command%==forceclearsh goto forceclearsh
+if %command%==getinfo goto getinfo
+if %command%==get goto get
+goto invalidcommand
+rem invalidcommand >
+:invalidcommand
+echo [40;31mW[40;33mh[40;32mo[40;34mo[40;35mp[40;36ms, [40;31mInvalid Command!
+goto commandline
+:invalid
+echo [40;31mInvalid Option Detected
+echo [40;31mTry Again
+echo goto commandline
+rem commandvalues >
+:clearlist
+cls
+goto main
+:help
+echo Welcome To Nimbus Terminal.
+echo Made By NulBerry
+echo.
+echo *all commands are in lowercase*
+echo.
+echo help - Shows Commands.
+echo cls - Clears Prompt.
+echo search - Searches The Web [Google].
+echo searchgoogle - Searches With Google.
+echo searchedge - Searches With Micorsoft Edge.
+echo searchincog - Searches With Incognito Mode.
+echo startapp - Launches Downloaded Apps.
+echo start - Starts Files In The Same Folder.
+echo shutdownlan - Shutdown Prompt.
+echo setwinsize - Resize Window.
+echo setwinsizeshort - Makes Window Short.
+echo setwinsizelong - Makes Window Long.
+echo del - Deletes a file (textfile.txt).
+echo writetxt - Writes a Text File to a Present File, Or Creates A File.
+echo erasetxt - Cleares a Text File .
+:: end of help list
+goto commandline
+:search
+set search=
+set /p search=[[40;34mG[40;31mo[40;33mo[40;34mg[40;32ml[40;31me[40;37m]Enter Search Keyword:[40;33m
+if %search%==halt goto commandline
+goto searchg
+:searchg
+start https://www.google.com/search?q=%search% 
+echo [%search%] Searched With [Google]>> searchhistory.txt
+goto commandline
+
+:searchurl
+set searchurl=
+set /p searchurl=[[40;34mG[40;31mo[40;33mo[40;34mg[40;32ml[40;31me[40;37m]Enter Search Keyword:[40;33m
+if %searchurl%==halt goto commandline
+goto searchu
+:searchu
+start https://%searchurl% 
+echo [%searchurl%] Searched With [Google]>> searchhistory.txt
+goto commandline
+
+:searchedge
+set searchedge=
+set /p searchedge=[[40;34mEdge[40;37m]Enter Search Keyword:[40;33m
+if %c
+start https://www.bing.com/search?q=%searchedge%
+echo [%searchedge%] Searched With [MicroSoft Edge]>> searchhistory.txt
+goto commandline
+
+:startapp
+set startapp=
+set /p startapp=[EXE]Enter Application Name:[40;33m
+start %startapp%.exe
+goto commandline
+
+:start
+set start=
+set /p start=Enter File Name:[40;33m
+start %start%
+goto commandline
+
+:setsize
+set setsize=
+set /p setsize=[##,##]Set Window Size:[40;33m
+mode %setsize%
+goto main
+
+:setwinsizelong
+set setsize=85,55
+mode %setsize%
+goto main
+
+:setwinsizeshort
+set setsize=85,7
+mode %setsize%
+goto main
+
+:del
+set del=
+set /p del=[40;37mDelete A File in This Folder:[40;33m
+if %del%==halt goto commandline
+if %del%==searchhistory.txt goto errordel
+if %del%==searchhistory goto errordel
+if %del%==terminal.bat goto crazy
+goto verifydel
+:crazy
+echo [40;31mBro Your Deleting The Terminal File.
+echo Are You Okay?
+goto del
+:errordel
+echo [40;31mThis File Cannot Be Deleted!
+echo Please Try Another File
+goto del
+:verifydel
+del %del%
+echo Deleted %del%.
+goto commandline
+
+:writetxt
+set file=
+set /p file=[40;37m[.txt]Enter File Name, Existing or NonExisting:[40;33m
+if %file%==searchhistory.txt goto erroredit2
+if %file%==searchhistory goto erroredit2
+goto write
+:erroredit2
+echo [40;31mThis File Has Been Restricted From Being Edited!
+echo Please Try Another File!
+echo.
+goto writetxt
+echo.
+:write
+set text=
+set /p text=[40;37mEnter Text To Populate The File With:[40;33m
+:: writing the data to chosen file
+echo %date% - %time%>> %file%.txt
+echo %text%>> %file%.txt
+echo ㅤ>> %file%.txt
+echo.
+echo Writing. . . 
+timeout 5 >nul
+echo Written!
+timeout 2>nul
+goto commandline
+
+:erasetxt
+set file=
+set /p file=[40;37mEnter The File Name To Erase:[40;33m
+if exist %file%.txt goto filecheck
+if not exist %file%.txt goto notexists
+:notexists
+echo [40;31m%file%.txt does not exist.
+echo [40;31mPlease Try A Diffrent File!
+goto erasetxt
+:filecheck
+if %file%==searchhistory.txt goto erroredit
+if %file%==searchhistory goto erroredit
+goto verify
+:erroredit
+echo [40;31mThis File Has Been Restricted From Being Edited!
+echo Please Try Another File!
+echo.
+goto erasetxt
+:: erase the data
+:verify
+set verify=
+set /p verify=[40;37mAll Data Will Be Erased, Continue?(y/n):[40;33m
+if %verify%==y goto erasecont
+if %verify%==Y goto erasecont
+if %verify%==n goto abortederase
+if %verify%==N goto abortederase
+:erasecont
+echo ㅤ> %file%.txt
+echo Erasing File. . . 
+timeout 3 >nul
+echo Erased!
+goto commandline
+:abortederase
+echo Erase Aborted of File [%file%]
+echo No Data Erased
+goto commandline
+
+:startinstall
+echo Apps Available To Install
+echo 1) NetTool
+echo 2) PingTool
+echo 3) IP Tool
+echo 4) Custom Install
+set install=
+set /p install=:[40;33m
+if %install%==1 goto nettool
+if %install%==2 goto pingtool
+if %install%==3 goto iptool
+if %install%==4 goto custominstall
+goto invalid
+
+:custominstall
+
+:nettool
+start https://github.com/vyberr/nettool/archive/refs/heads/main.zip
+echo [40;37m--instructions--
+echo extract the file into the same folder as the terminal
+echo goto to terminal and start the application
+echo using [nettool.bat] or [nettool]
+goto commandline
+
+:pingtool
+start https://github.com/vyberr/pingtool/archive/refs/heads/main.zip
+echo [40;37m--instructions--
+echo extract the file into the same folder as the terminal
+echo goto to terminal and start the application
+echo using [start pingtool.bat] or [pingtool]
+goto commandline
+
+:iptool
+start https://github.com/vyberr/iptool/archive/refs/heads/main.zip
+echo [40;37m--instructions--
+echo extract the file into the same folder as the terminal
+echo goto to terminal and start the application
+echo using "start iptool.bat" or "iptool"
+goto commandline
+
+:launchip
+if exist iptool.bat goto ipcont
+if not exist iptool.bat goto iperror
+:iperror
+echo [40;37mIPTool Has Not Been Installed
+echo Use "install" command to download
+goto commandline
+:ipcont
+start iptool.bat
+goto commandline
+:launchnet
+if exist nettool.bat goto netcont
+if not exist nettool.bat goto neterror
+:neterror
+echo [40;37mNetTool Has Not Been Installed
+echo Use "install" command to download
+goto commandline
+:netcont
+start nettool.bat
+goto commandline
+:launchping
+if exist pingtool.bat goto pingcont
+if not exist pingtool.bat goto pingerror
+:pingerror
+echo [40;37mPingTool Has Not Been Installed
+echo Use "install" command to download
+goto commandline
+:pingcont
+start pingtool.bat
+goto commandline
+
+:allfiles
+dir
+goto commandline
+
+:secret
+echo [40;37mWait What...
+echo.
+echo thanks for freeing me from this terminal!
+echo.
+echo I Will Grant You 1 Wish
+set secretwish=
+set /p secretwish= Enter Your Wish!:[40;33m
+echo.
+echo Whoops, We Ran Into An Error Processing: %secretwish%
+goto commandline
+
+:debugmode
+mode 80,40
+echo debug mode activated
+echo.
+echo Messing With Debug Settings Can and Will Alter The State Of Performance!
+pause>nul
+goto main
+
+:scriptwrite
+
+:forceclearsh
+echo Last Erase %date% %time%> searchhistory.txt
+echo ㅤ>> searchhistory.txt
+echo [40;31mcleared!
+goto commandline
+
+:get
+echo [40;37mCheck Downloaded Packets
+set get=
+set /p get=:[40;33m
+if %get%==iptool goto check
+if %get%==pingtool goto check
+:check
+if exist %get% echo [40;37m[%get%] Status [[40;32mAvailable[40;37m]
+if not exist %get% echo [40;37m[%get%] Status [[40;31mNot Available[40;37m]
+goto commandline
+
+
+:getinfo
+echo.
+echo.
+echo [40;34m                  .-~*~--,.   .-.
+echo [40;34m          .-~-. ./OOOOOOOOO\.'OOO`9~~-.                     [40;37mOS:      NIMBUS_OS
+echo [40;34m        .`OOOOOO.OOM.OLSONOOOOO@@OOOOOO\                    [40;37mDev:     NulBerry
+echo [40;34m       /OOOO@@@O[40;33m╔╗╔╦╔╦╗╔╗ ╦ ╦╔═╗[40;34m@@@@OOOO`.                  [40;37mCreated: 2022-05-03
+echo [40;34m        OO@@@WWW[40;33m║║║║║║║╠╩╗║ ║╚═╗[40;34m@@@@@@OOOO).                [40;37mUser:    %username%
+echo [40;34m     .-'OO@@@@WW[40;33m╝╚╝╩╩ ╩╚═╝╚═╝╚═╝[40;34mW@@@@@OOOOOO}               [40;37mDesk:    %computername%
+echo [40;34m    /OOO@@O@@@@W@@@@@OOWWWWWOOWOO@@@OOO@@@OO                [40;37mVers:    %version% 
+echo [40;34m   lOOO@@@OO@@@WWWWWWW\OWWWO\WWWOOOOOO@@@O.'                [40;37mRoot:    github.com/vyberr
+echo [40;34m    \OOO@@@OOO@@@@@@OOW[40;33m\     \[40;34mWWWW@@@@@@@O'.
+echo [40;34m     `,OO@@@OOOOOOOOOOWW[40;33m\     \[40;34mWWWW@@@@@@OOO)
+echo [40;34m      \,O@@@@@OOOOOOWWWWW[40;33m\     \[40;34mWW@@@@@OOOO.'
+echo [40;34m        `~c~8~@@@@WWW@@W[40;33m\       \[40;34mWOO \UO-~'
+echo [40;34m             (OWWWWWW@/\W[40;33m\    ___\[40;34mWO)
+echo [40;34m               `~-~''     [40;33m\   \[40;34mWW=*'
+echo [40;33m                         __\   \
+echo [40;33m                         \      \
+echo [40;33m                          \    __\
+echo [40;33m                           \  \
+echo [40;33m                            \ \
+echo [40;33m                             \ \
+echo [40;33m                              \\
+echo [40;33m                               \\
+echo [40;33m                                \
+echo [40;33m                                 \
+echo.
+echo.
+goto commandline
